19d2a73cc04dd253ab2912082f30d184e5089f75
Kelton <Kelton@AIRPLANE>
2022-08-04 12:06:51 -0300

    Criação das bernias entidade, enum e a bernias master objetodevalor

---
+++ QuickBuy.Dominio/Entidades/ItemPedido.cs
@@ -0,0 +1,9 @@
+﻿namespace QuickBuy.Dominio.Entidades
+{
+    public class ItemPedido
+    {
+        public int Id { get; set; }
+        public int PedidoId { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
