58bbcdd2bd2f2fced290aeb3ea9286bafa35def5
Kelton <Kelton@AIRPLANE>
2022-08-04 13:01:45 -0300

    Implantando validação bernia nas classes

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,10 +1,11 @@
 ﻿using QuickBuy.Dominio.ObjetoDeValor;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Pedido
+    public class Pedido : Entidade
     {
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
@@ -19,5 +20,17 @@ namespace QuickBuy.Dominio.Entidades
         public FormaPagamento FormaPagamento { get; set; }
         public ICollection<ItemPedido> ItensPedidos { get; set; }
 
+        public override void Validate()
+        {
+            if(ItensPedidos.Any())
+            {
+                LimparMensagemValidacao();
+                AdicionarCritica("Crítica: Pedido não pode ficar sem item de pedido");
+            }
+            if(string.IsNullOrEmpty(CEP))
+            {
+                AdicionarCritica("Crítica: CEP deve estar preenchido");
+            }
+        }
     }
 }
